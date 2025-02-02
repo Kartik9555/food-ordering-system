b641a6f625a1200987185681d82364def1bd8263
kartikeshwar.k <kartik9555@gmail.com>
2025-02-02 12:04:08 +0100

    Added entities, value objects and exceptions

--- common/common-domain/src/main/java/com/food/ordering/system/domain/valueobject/Money.java
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/valueobject/Money.java
@@ -12,6 +12,7 @@ import java.math.RoundingMode;
 @RequiredArgsConstructor
 public class Money {
     private final BigDecimal amount;
+    public static final Money ZERO = new Money(BigDecimal.ZERO);
 
     public boolean isGreaterThanZero() {
         return this.amount != null && this.amount.compareTo(BigDecimal.ZERO) > 0;
