70a9878f5f609322637174f1de003beef1c3761c
kartikeshwar.k <kartik9555@gmail.com>
2025-02-06 23:54:14 +0100

    Added Order Customer repository and adapter implementations

---
+++ order-service/order-dataaccess/src/main/java/com/food/ordering/system/order/service/dataaccess/customer/entity/CustomerEntity.java
@@ -0,0 +1,18 @@
+package com.food.ordering.system.order.service.dataaccess.customer.entity;
+
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "order_customer_m_view", schema = "customer")
+public class CustomerEntity {
+    @Id
+    private UUID id;
+}
