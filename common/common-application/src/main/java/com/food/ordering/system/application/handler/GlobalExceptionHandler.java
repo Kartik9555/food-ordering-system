0056c5cee569e9f265398935a4504fa07659fddc
kartikeshwar.k <kartik9555@gmail.com>
2025-02-06 22:25:50 +0100

    Implemented controller advice and global exception handler

---
+++ common/common-application/src/main/java/com/food/ordering/system/application/handler/GlobalExceptionHandler.java
@@ -0,0 +1,48 @@
+package com.food.ordering.system.application.handler;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.ValidationException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.stream.Collectors;
+
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(value = {Exception.class})
+    public ErrorDTO handleException(Exception exception) {
+        log.error(exception.getMessage(), exception);
+        return ErrorDTO.builder()
+                .code(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase())
+                .message("Unexpected error!")
+                .build();
+    }
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(value = {Exception.class})
+    public ErrorDTO handleException(ValidationException exception) {
+        String message = exception instanceof ConstraintViolationException ? extractViolationsFromException((ConstraintViolationException) exception) : exception.getMessage();
+        log.error(message, exception);
+        return ErrorDTO.builder()
+                .code(HttpStatus.BAD_REQUEST.getReasonPhrase())
+                .message(message)
+                .build();
+    }
+
+    private String extractViolationsFromException(ConstraintViolationException exception) {
+        return exception.getConstraintViolations()
+                .stream()
+                .map(ConstraintViolation::getMessage)
+                .collect(Collectors.joining("--"));
+    }
+}
