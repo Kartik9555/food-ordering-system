a348c2a66bf22dea80571c48417edf59256ba644
kartikeshwar.k <kartik9555@gmail.com>
2025-02-18 23:12:06 +0100

    Implemented payment Data Access module

---
+++ payment-service/payment-dataaccess/src/main/java/com/food/ordering/system/payment/service/dataaccess/creditentry/entity/CreditEntryEntity.java
@@ -0,0 +1,35 @@
+package com.food.ordering.system.payment.service.dataaccess.creditentry.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "credit_entry")
+@Entity
+public class CreditEntryEntity {
+    @Id
+    private UUID id;
+    private UUID customerId;
+    private BigDecimal totalCreditAmount;
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof CreditEntryEntity that)) return false;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
